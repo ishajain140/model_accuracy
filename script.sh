ba51ad7d7a229e66b58b4b86a1a5382feda379d7
isha <ishajain140@gmail.com>
2020-05-30 01:25:03 +0530

    final

--- script.sh
+++ script.sh
@@ -4,9 +4,9 @@ then
 if sudo docker ps -a | grep cnn_os
 then 
 sudo docker rm -f cnn_os
-sudo docker run -dit -v /var/lib/jenkins/workspace/github_pull:/mlops --name cnn_os mlopsimage:v1
+sudo docker run -dit -v /home/isha/mlops/project:/mlops --name cnn_os mlopsimage:v1
 else
-sudo docker run -dit -v /var/lib/jenkins/workspace/github_pull:/mlops --name cnn_os mlopsimage:v1
+sudo docker run -dit -v /home/isha/mlops/project:/mlops --name cnn_os mlopsimage:v1
 fi
 fi
 
