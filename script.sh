8c6fb12491845136fbb24dc3fabd01a066fd3980
isha <ishajain140@gmail.com>
2020-05-29 17:29:33 +0530

    second

--- script.sh
+++ script.sh
@@ -4,12 +4,12 @@ then
 if sudo docker ps -a | grep cnn_os
 then 
 sudo docker rm -f cnn_os
-sudo docker run -dit -v /var/lib/jenkins/workspace/github_pull:/mlops --name cnn_os mlopsimage:v1
+sudo docker run -dit -v /var/lib/jenkins/workspace/github_pull:/project --name cnn_os mlopsimage:v1
 else
-sudo docker run -dit -v /var/lib/jenkins/workspace/github_pull:/mlops --name cnn_os mlopsimage:v1
+sudo docker run -dit -v /var/lib/jenkins/workspace/github_pull:/project --name cnn_os mlopsimage:v1
 fi
 fi
 
-sudo docker rm -f /mlops/accuracy_check.txt
-sudo docker exec cnn_os  python3 /mlops/maincode.py
+sudo docker rm -f /project/accuracy_check.txt
+sudo docker exec cnn_os  python3 /project/maincode.py
 
